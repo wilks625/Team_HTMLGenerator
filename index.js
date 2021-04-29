559de59be5400ce7b30594e0323f66ab7dac5dcf
Stephen Yacas <syacas123@gmail.com>
2021-04-28 23:09:03 -0400

    edits to index.js

--- index.js
+++ index.js
@@ -1,2 +1,112 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const util = require('util');
+
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+const questions = [
+    {
+        type: 'list',
+        name: 'jobType',
+        message: 'Which role card would you like to build?',
+        choices: ["Manager", "Engineer", "Intern"]
+      },
+      {
+        type: 'input',
+        name: 'employeeName',
+        message: "What is your employee's name?",
+      },
+      {
+        type: 'input',
+        name: 'employeeID',
+        message: "What is your employee's ID?",
+      },
+      {
+        type: 'input',
+        name: 'employeeEmail',
+        message: "What is your employee's email address?",
+      },
+];
+
+
+// let generatehtmlPage = teamObj => {
+//     console.log('team object', teamObj)
+
+//    // set card to empty
+//     let htmlCard = ""
+
+//     //loop over array of objects
+
+//     for(let i = 0; i < teamObj.length; i++){
+//         let finalPrompt = teamObj[i].office  teamObj[i].gitHub  teamObj[i].school;
+//         let keys = Object.keys(teamObj[i]);
+//         let lastKey = keys[4];
+//         let finalOption = lastKey + ":" + finalPrompt
+
+//         if (lastKey === undefined){
+//             finalOption = "";
+
+//         } else if (lastKey === 'gitHub'){
+//             finalOption = (GitHub : <a href = 'https://www.github.com/${teamObj[i].gitHub}'> ${teamObj[i].gitHub}</a>)
+//             console.log(finalOption)
+//         }
+//         else {
+//             console.log(finalOption)
+//         }
+//     }
+
+const generateHTML = (userInput) =>
+  `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/Develop/dist/css/style.css">
+    <title>Document</title>
+  </head>
+  <body>
+      <header style="background-color:red; color: white; text-align: center; font-size: large;">My Team</header>
+  
+  <div class="card" style="width: 18rem;">
+    <div class="container">
+      <div style="background-color:rgb(66, 57, 240); color: white;">
+          <h4 class="display-6">${userInput.employeeName}</h4>
+          <h4>${userInput.jobType}.</h4>
+      </div> 
+      <ul class="list-group">
+        <li class="list-group-item">ID: ${userInput.employeeID}</li>
+        <li class="list-group-item">Email: ${userInput.employeeEmail}</li>
+        
+      </ul>
+    </div>
+  </div>
+  
+  </body>
+  </html>`;
+
+
+const promptUser = () => {
+    return inquirer.prompt(questions)
+}
+
+// Function to write README file
+const writeToFile = (userInput) => {
+    fs.writeFile('README.md', userInput, (error) =>
+    error ? console.log('Error!') : console.log('Success!'));
+}
+
+// Function to initialize app
+const init = () => {
+    promptUser()
+      .then((userInput) => writeFileAsync('employeeRoster.html', generateHTML(userInput)))
+      .then(() => console.log('Successfully wrote to employeeRoster.html!!!'))
+      .catch((err) => console.error(err));
+  };
+
+// Function call to initialize app
+init();
